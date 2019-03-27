b7c5dc5c9c751c82875159898113e4bacadcae9b
sylhare <sylhare@outlook.com>
2019-03-27 08:03:54 -0400

    Update .gitignore

---
+++ Exercices/src/tests/RectangleJunit5Test.kt
@@ -0,0 +1,21 @@
+/*package tests
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RectangleJunit5Test {
+
+    @org.junit.jupiter.api.Test
+    fun perimeter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun area() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun hasRightAngle() {
+    }
+    @org.junit.jupiter.api.Test
+    fun `valid parameters` {}
+}*/
+
