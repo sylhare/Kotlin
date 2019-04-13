387032ec7035471d6e8066e8276e8537a1ac2555
sylhare <sylhare@outlook.com>
2019-04-13 19:32:45 -0400

    Update polygon tests

--- Exercices/src/test/kotlin/exercises/PolygonTest.kt
+++ Exercices/src/test/kotlin/exercises/PolygonTest.kt
@@ -7,38 +7,38 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertFailsWith
 import kotlin.test.assertNull
 
-class PolygonTest {
+internal class PolygonTest {
 
     @Test
-    fun hasRightPerimeter() {
+    internal fun hasRightPerimeter() {
         assertEquals(10.0, Polygon(2, 3).perimeter())
     }
 
-    @Test fun hasRightArea() {
+    @Test internal fun hasRightArea() {
         assertEquals(12.0, Polygon(3, 4).area())
     }
 
-    @Test fun hasARightAngle() {
+    @Test internal fun hasARightAngle() {
         assertTrue(Polygon(3, 4).hasRightAngle())
     }
 
-    @Test fun validParameters() {
-        assertFailsWith<IllegalArgumentException> {
-            Polygon(0,0)
-        }
+    @Test internal fun validParameters() {
+        assertFailsWith<IllegalArgumentException> { Polygon(0,0) }
+        assertFailsWith<IllegalArgumentException> { Polygon(-2 ,1) }
+        assertFailsWith<IllegalArgumentException> { Polygon(1,-2) }
     }
 
-    @Test fun canBeScaled() {
+    @Test internal fun canBeScaled() {
         assertEquals(20.0, Polygon.rectangle(2, 3).scale(2).perimeter())
     }
 
-    @Test fun `Square and rectangles`() {
+    @Test internal fun `Square and rectangles`() {
         assertEquals(square(2).perimeter(), rectangle(2, 2).perimeter())
         assertEquals(square(2).area(), rectangle(2, 2).area())
         assertEquals(square(2).scale(2).perimeter(), rectangle(2, 2).scale(2).perimeter())
     }
 
-    @Test fun `Get the biggest rectange`() {
+    @Test internal fun `Get the biggest rectange`() {
         assertEquals(rectangle(4,4).area(),
                 Comparable.greatestOf(arrayOf(rectangle(4,1), rectangle(1,2 ), rectangle(1, 3), rectangle(4, 4)))?.area())
         assertNull(Comparable.greatestOf(emptyArray<Polygon>()))
