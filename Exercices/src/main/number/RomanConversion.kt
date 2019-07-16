6d5a619fe36a1941c1a1a83ab07359b41c677528
sylhare <sylhare@outlook.com>
2019-07-16 10:21:53 -0400

    Add Roman Conversation

---
+++ Exercices/src/main/number/RomanConversion.kt
@@ -0,0 +1,25 @@
+package number
+
+import kotlin.math.pow
+
+class RomanConversion {
+
+    companion object {
+        private const val MAX = 3
+        private val visuals = mapOf(1 to "I", 5 to "V", 10 to "X", 50 to "L", 100 to "C", 500 to "D", 1000 to "M")
+
+        private fun base(zeros: Int) = 1 * 10.0.pow(zeros).toInt()
+        private fun decompose(n: Int) = (MAX downTo 0).map { (n / base(it)) % 10 }
+
+        private fun compose(value: Int, base: Int): String {
+            if (base == 1000) return visuals.getValue(base).repeat(value)
+            if (value == 9) return visuals[base] + visuals[base * 10]
+            if (value >= 5) return visuals[base * 5] + visuals.getValue(base).repeat(value - 5)
+            if (value == 4) return visuals[base] + visuals[base * 5]
+            return visuals.getValue(base).repeat(value)
+        }
+
+        fun of(n: Int) = decompose(n).mapIndexed { i, x -> compose(x, base(MAX - i)) }.joinToString("")
+    }
+
+}
--- Exercices/src/test/number/RomanUnitTest.kt
+++ Exercices/src/test/number/RomanUnitTest.kt
@@ -54,5 +54,19 @@ internal class RomanUnitTest {
     }
 
 
+    @Test
+    fun romanUnitConvert() {
+
+        assertEquals( "MMM", RomanConversion.of(3000))
+        assertEquals( "MCMXCIX", RomanConversion.of(1999))
+        assertEquals( "CXXVIII", RomanConversion.of(128))
+        assertEquals( "XLIX", RomanConversion.of(49))
+        assertEquals( "I", RomanConversion.of(1))
+        assertEquals( "III", RomanConversion.of(3))
+        assertEquals( "IV", RomanConversion.of(4))
+        assertEquals( "VII", RomanConversion.of(7))
+        assertEquals( "IX", RomanConversion.of(9))
+        assertEquals( "X", RomanConversion.of(10))
+    }
 
 }
