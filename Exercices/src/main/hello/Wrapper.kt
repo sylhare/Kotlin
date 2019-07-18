f18f9add76a9a289ae4a132be28de9b227a79841
sylhare <sylhare@outlook.com>
2019-07-18 11:15:35 -0400

    Update Input from previous Experiment

--- Exercices/src/main/hello/Wrapper.kt
+++ Exercices/src/main/hello/Wrapper.kt
@@ -1,10 +1,10 @@
 package hello
 
-internal fun List<Experiments>.concatenate() = Experiments.concatenate(this)
+internal fun List<Input>.concatenate() = Input.concatenate(this)
 internal fun List<String>.concatRegex() = this.takeWhile { it.contains("\\d+".toRegex()) }.joinToString("")
 internal fun List<String>.concatOther() = this.takeWhile { it.toIntOrNull() != null }.joinToString("")
 fun List<String>.concatNotInternal() = this.joinToString("") { it }.takeWhile { it.isDigit() }
 internal fun List<String>.concat() = this.joinToString("") { it }.takeWhile { it.isDigit() }
 
-val Number.exp get() = Experiments(this.toString())
+val Number.exp get() = Input(this.toString())
 fun  List<Number>.toExp() = this.forEach { it.exp }
--- Exercices/src/test/hello/InputTest.kt
+++ Exercices/src/test/hello/InputTest.kt
@@ -6,11 +6,11 @@ import kotlin.test.assertFalse
 import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
-internal class ExperimentsTest {
+internal class InputTest {
     @Test
     fun compareTo() {
-        assertEquals(Experiments("s"), Experiments("s"))
-        assertNotEquals(Experiments("a"), Experiments("s"))
+        assertEquals(Input("s"), Input("s"))
+        assertNotEquals(Input("a"), Input("s"))
     }
 
     @Test
@@ -25,8 +25,13 @@ internal class ExperimentsTest {
 
     @Test
     fun has() {
-        assertTrue(Experiments.has(listOf(1.exp, 2.exp, 3.exp, 4.exp), '2'))
-        assertFalse(Experiments.has(listOf(1.exp, 2.exp, 3.exp, 4.exp), 'e'))
+        assertTrue(Input.has(listOf(1.exp, 2.exp, 3.exp, 4.exp), '2'))
+        assertFalse(Input.has(listOf(1.exp, 2.exp, 3.exp, 4.exp), 'e'))
         listOf(1,2,3,4).toExp()
+    }
+
+    @Test
+    fun numberToList() {
+        assertEquals(listOf(5, 4, 3, 2, 1), Input.numberToList(12345))
     }
 }
