784087af193d119eecad699d0efbff9555c64177
sylhare <sylhare@outlook.com>
2019-11-15 23:07:04 -0500

    Add from list remove and filter anagrams

--- Exercices/src/main/kotlin/input/Anagram.kt
+++ Exercices/src/main/kotlin/input/Anagram.kt
@@ -11,3 +11,6 @@ class Anagram {
 
 internal fun String.alphaSort() = this.toCharArray().sorted().joinToString(separator = "")
 internal fun String.isAnagram(other: String) = this.alphaSort() == other.alphaSort()
+internal fun List<String>.removeAllAnagrams() = this.filter { that -> this.map { that.isAnagram(it) }.filter { it }.size == 1 }
+internal fun List<String>.removeAnagramDuplicates() = this.associateBy ({it.alphaSort()}, {it}).values.toList()
+internal fun List<String>.filterAnagrams() = this.reversed().removeAnagramDuplicates().reversed()
--- Exercices/src/test/kotlin/input/AnagramTest.kt
+++ Exercices/src/test/kotlin/input/AnagramTest.kt
@@ -14,9 +14,18 @@ class AnagramTest {
         assertEquals(5, Anagram.filter(listOf("nanana", "nana", "panama", "bac", "ananas", "cab")).size)
         assertEquals(4, Anagram.filter(listOf("cat", "tac", "hello", "bonjour", "allo", "loal")).size)
     }
+
     @Test
     fun isAnagramTest(){
         assertTrue("cat".isAnagram("tac"))
         assertFalse("nanan".isAnagram("nana"))
+    }
+
+    @Test
+    fun anagramsAreFilteredSameOutput(){
+        var words = listOf("cat", "tac", "hello", "bonjour", "allo", "loal")
+        assertEquals(listOf("hello", "bonjour"), words.removeAllAnagrams())
+        assertEquals(listOf("tac", "hello", "bonjour", "loal"), words.removeAnagramDuplicates())
+        assertEquals(listOf("cat", "hello", "bonjour", "allo"), words.filterAnagrams())
     }
 }
