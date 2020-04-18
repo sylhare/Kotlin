4699a45019150a6d91fb70b3dddef4ae91a2447c
sylhare <sylhare@outlook.com>
2020-04-18 17:42:22 -0400

    Update gradle

--- Exercices/build.gradle.kts
+++ Exercices/build.gradle.kts
@@ -1,11 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.71"
     jacoco
+    publishing
+    application
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
+application.mainClassName = "examples.HelloKt"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
 repositories {
     jcenter()
@@ -13,8 +21,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
+
+    constraints {
+        api("com.google.guava:guava") {
+            version{ rejectAll() } // To say you don't want guava in the project
+        }
+    }
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.71")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.71")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
@@ -23,7 +37,7 @@ dependencies {
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useJUnitPlatform() // To sat to use the junit engine while test
     testLogging {
         events("passed", "skipped", "failed")
     }
