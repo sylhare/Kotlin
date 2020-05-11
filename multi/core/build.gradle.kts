99de237b05f2a177c5d6dba25a9b0fcaf294722d
sylhare <sylhare@outlook.com>
2020-05-11 08:53:49 -0400

    Add publish gradle

--- multi/core/build.gradle.kts
+++ multi/core/build.gradle.kts
@@ -2,4 +2,34 @@ plugins {
     kotlin("jvm")
 }
 
-version = "1.0.0"
+val sourcesJar by tasks.creating(Jar::class) {
+    dependsOn(JavaPlugin.CLASSES_TASK_NAME)
+    from(sourceSets.main.get().allSource)
+    from(sourceSets.test.get().allSource) // because we want source test too
+}
+
+configurations {
+    create("mock")
+}
+
+tasks.register<Jar>("testArchive") {
+    archiveBaseName.set("core-test")
+    from(project.the<SourceSetContainer>()["test"].output)
+    include("examples/mocks/**")
+}
+
+artifacts {
+    add("mock", tasks["testArchive"])
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "examples"
+            artifactId = "core"
+            version = "1.0"
+            artifact(sourcesJar)
+            artifact("$buildDir/libs/core.jar")
+        }
+    }
+}
