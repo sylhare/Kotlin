92cee798b8fd4f81e9106902484dd50cfe039453
sylhare <sylhare@outlook.com>
2021-03-02 11:46:22 -0500

    Refactor TrainNode

--- Exercices/src/main/kotlin/train/TrainNode.kt
+++ Exercices/src/main/kotlin/train/TrainNode.kt
@@ -53,35 +53,30 @@ fun insert(root: TrainNode?, data: TrainStep<TrainNetwork>, index: Int): TrainNo
         else -> root.also { it.right = insert(it.right, data, index) }
     }
 
-fun trainPath(root: TrainNode, switch: List<String>): List<TrainNetwork> {
+fun trainPath(root: TrainNode, switch: List<String>): List<TrainNetwork> = when(root.childCount()) {
+    1 -> listOf(root.value) + trainPath(root.right!!, switch)
+    2 -> selectSwitchPath(root, switch)
+    else -> listOf(root.value)
+}
+
+
+fun selectSwitchPath(root: TrainNode, switch: List<String>): MutableList<TrainNetwork> {
     var currentSwitch = switch
-    if (root.value is Switch) {
-        currentSwitch = if (currentSwitch.size > 1) switch.drop(1) else listOf()
-        return when (root.value.info) {
-            switch[0] -> listOf(root.value) + trainPath(root.right!!, currentSwitch)
-            else -> listOf()
-        }
-    }
-    return when (root.childCount()) {
-        1 -> listOf(root.value) + trainPath(root.right!!, switch)
-        2 -> {
-            val result = mutableListOf(root.value)
-            var leftPath = listOf<TrainNetwork>()
-            var rightPath = listOf<TrainNetwork>()
-            while (leftPath.isEmpty() && rightPath.isEmpty()) {
-                leftPath = trainPath(root.left!!, currentSwitch)
-                rightPath = trainPath(root.right!!, currentSwitch)
-                if (leftPath.isEmpty() && rightPath.isEmpty()) {
-                    result += listOf(Switch(switch[0]), RailSection.invalid)
-                    currentSwitch = switch.drop(1)
-                } else break
-            }
-            result + leftPath + rightPath
-        }
-        else -> listOf(root.value)
-    }
+    val result = mutableListOf(root.value)
+    do {
+        val leftPath = switchPath(root.left!!, currentSwitch)
+        val rightPath = switchPath(root.right!!, currentSwitch)
+        if (leftPath.isEmpty() && rightPath.isEmpty()) {
+            result += listOf(Switch(switch[0]), RailSection.invalid)
+            currentSwitch = switch.drop(1)
+        } else result += leftPath + rightPath
+    } while (leftPath.isEmpty() && rightPath.isEmpty())
+    return result
+}
 
-
+fun switchPath(root: TrainNode, switch: List<String>) = when {
+    root.value is Switch && root.value.info == switch[0] -> listOf(root.value) + trainPath(root.right!!, switch.drop(1))
+    else -> listOf()
 }
 
 fun printLevelOrderTree(node: TrainNode) {
